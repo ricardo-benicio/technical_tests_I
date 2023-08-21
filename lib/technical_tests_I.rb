1735408f777e1cdc301bad713cf0f760ee8ca32e
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-21 14:29:24 -0300

    Initial commit

---
+++ lib/technical_tests_I.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "technical_tests_I/version"
+
+module TechnicalTestsI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/technical_tests_I_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TechnicalTestsI do
+  it "has a version number" do
+    expect(TechnicalTestsI::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
