af209277a14234d13fec3ebea1d42d824e62b58e
ricardo-benicio <rikrdoofelipe@outlook.com>
2024-01-18 15:00:36 -0300

    build: new project.

--- spec/factories/word_factory.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :word do
-    name { FFaker::Lorem.word }
-
-    trait(:palindrome) { name { FFaker::Lorem.word.reverse } }
-  end
-end
